3b253f2a4f6423c155eb100c91db31250d3982b5
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-10 16:21:02 +0600

    Update and rename scripts/commands/0goibot.js to scripts/commands0autocation.js

---
+++ scripts/commands0autocation.js
@@ -0,0 +1,13 @@
+module.exports.config = {
+  name: "autotime",
+  version: "1.0.0",
+  permission: 0,
+  prefix: true,
+  credits: "Nayan",
+  description: "msg",
+  category: "awto send msg", 
+  usages: "",
+  cooldowns: 5,
+  dependencies: {
+	}
+};
